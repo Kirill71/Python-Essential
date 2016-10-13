d5334553263843b7f9c3d2fa00490745bf580ba3
Kirill71 <extazy1701@gmail.com>
2016-10-13 17:20:39 +0300

    Encapsulation in Python

--- Encapsulation/additionalTask/main.py
+++ Encapsulation/additionalTask/main.py
@@ -1,6 +1,9 @@
 from Encapsulation.additionalTask import AvtoSaloon, Avto
 
 
+
+
+
 def main():
     saloon = AvtoSaloon.AvtoSaloon()
     saloon.add_car(Avto.Avto("BMW", "X5", 1000000))
@@ -9,5 +12,8 @@ def main():
     print(saloon)
     saloon.sale_car(Avto.Avto("Lada", "Kalina", 10))
     print(saloon)
+
+
+
 
 main()
