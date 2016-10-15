53c5f0633bf50448a08f9392965a53d26b31d1e1
Kirill71 <extazy1701@gmail.com>
2016-10-15 21:31:14 +0300

    Exceptions in Python

---
+++ Exceptions/additionalTask/myException.py
@@ -0,0 +1,19 @@
+class MyException(Exception):
+
+    def __init__(self, value):
+        super().__init__(value)
+        print(value)
+
+
+
+def raise_exception():
+        raise MyException("bla-bla-bla")
+
+def main():
+    try:
+        raise_exception()
+    except MyException as e:
+        print("catch")
+
+if __name__ == '__main__':
+    main()
